9f00b11bf4f58e0dfa5a7a25510f739a3e917c51
Siyana <72863822+Kastalche@users.noreply.github.com>
2023-02-03 21:59:37 +0200

    Three roots

---
+++ Roots/Assets/RootManagerScript.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RootManagerScript : MonoBehaviour
+{
+    public GameObject[] roots;
+    // Start is called before the first frame update
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //roots[1].color= false;
+    }
+}
