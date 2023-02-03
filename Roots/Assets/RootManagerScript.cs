7d1a92bc7f4b2781a3b2c0a5143324298b023d31
JNayden <56478030+JNayden@users.noreply.github.com>
2023-02-03 22:44:41 +0200

    colorized roots(ready to broke)

--- Roots/Assets/RootManagerScript.cs
+++ Roots/Assets/RootManagerScript.cs
@@ -5,11 +5,25 @@ using UnityEngine;
 public class RootManagerScript : MonoBehaviour
 {
     public GameObject[] roots;
+    public Color32 cBrown = new Color32(139, 69, 19, 255);
+    public Color32 cBlack = new Color32(0, 0, 0, 255);
     // Start is called before the first frame update
-    
+    Vector3 testvector = new Vector3(0, 1, 1);
     void Start()
     {
-        
+        int randomRoot = Random.Range(0, roots.Length);
+        for (int i = 0; i < roots.Length; i++)
+        {
+            if (i == randomRoot)
+            {
+                roots[i].SetActive(true);
+                roots[i].GetComponent<SpriteRenderer>().color = cBlack;
+            }
+            else
+            {
+                roots[i].GetComponent<SpriteRenderer>().color = cBrown;
+            }
+        }
     }
 
     // Update is called once per frame
